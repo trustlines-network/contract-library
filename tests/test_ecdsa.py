094d508148680701f9767709bbf1acac767f75b8
dakingha69 <dongha.kim210@gmail.com>
2019-01-22 14:27:59 +0100

    Add commit hash and MIT license to ECDSA tests

--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -4,7 +4,31 @@ from web3 import Web3
 
 """
 NOTE Tests are ported from OpenZeppelin
-(https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/cryptography/ECDSA.test.js)
+(https://github.com/OpenZeppelin/openzeppelin-solidity/blob/3e82db2f6f8b8bdc05b22fb32bc2842ea58f001d/test/cryptography/ECDSA.test.js)
+
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Smart Contract Solutions, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 TEST_MESSAGE = Web3.sha3(text='OpenZeppelin')
